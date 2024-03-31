ee64d6b1759a9275250a2a4cf46884efac0705da
matin.mortazavi.a <matin.mortazavi.a@gmail.com>
2024-03-31 12:38:07 +0330

    feat (navbar) : navbar component added

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { appleImg } from "@/utils";
+import { appleImg, searchImg } from "@/utils";
+import { bagImg } from "../../utils";
+import { navLists } from "../../constants";
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="apple image" width={14} height={18} />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((navItem, i) => (
+            <div
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              key={i}
+            >
+              {navItem}
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-7 max-sm:justify-end max-sm:flex-1 items-baseline">
+          <img src={searchImg} alt="search image" width={18} height={18} />
+          <img src={bagImg} alt="bag image" width={18} height={18} />
+        </div>
       </nav>
     </header>
   );
